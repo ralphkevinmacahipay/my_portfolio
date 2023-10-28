bb721fcc54f2c42c1f07f2b2004729821d474478
Ralph Kevin Rynard E. Macahipay <53963085+ralphkevinmacahipay@users.noreply.github.com>
2023-10-28 13:17:04 +0800

    nav_bar responsive

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:responsive_framework/responsive_framework.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePageResponsive extends StatelessWidget {
   const HomePageResponsive({super.key});
@@ -8,17 +10,38 @@ class HomePageResponsive extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         title: ResponsiveBreakpoints.of(context).isMobile
-            ? const Row(children: [Text("Mobile")])
+            ? Row(children: [
+                IconButton(onPressed: () {}, icon: const Icon(Icons.menu))
+                    .marginOnly(right: context.percentWidth * 5),
+                const Text("Home"),
+                const Spacer(),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.dark_mode))
+              ])
             : ResponsiveBreakpoints.of(context).isTablet
-                ? const Row(children: [Text("Tablet")])
-                : const Row(children: [
-                    Text("Ralph.dart"),
-                    Spacer(),
-                    Text("Home"),
-                    Text("Services"),
-                    Text("Projects"),
-                    Text("Contact")
+                ? Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                        const Text("Home"),
+                        const Text("Ralph.dart"),
+                        IconButton(
+                            onPressed: () {}, icon: const Icon(Icons.menu))
+                      ])
+                : Row(children: [
+                    IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.light_mode))
+                        .marginOnly(right: context.percentWidth * 5),
+                    const Text("Ralph.dart"),
+                    const Spacer(),
+                    const Text("Home")
+                        .marginOnly(right: context.percentWidth * 5),
+                    const Text("Services")
+                        .marginOnly(right: context.percentWidth * 5),
+                    const Text("Projects")
+                        .marginOnly(right: context.percentWidth * 5),
+                    const Text("Contact")
                   ]),
       ),
     );
