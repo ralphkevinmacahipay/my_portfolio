2feea3dcdd79df22f83b99922cce93c20fdd2b40
Ralph Kevin Rynard E. Macahipay <53963085+ralphkevinmacahipay@users.noreply.github.com>
2023-11-08 17:19:50 +0800

    done project initial

--- lib/play_ground.dart
+++ lib/play_ground.dart
@@ -1,1 +1,119 @@
+import 'package:flutter/material.dart';
 
+void main() {
+runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+// This widget is the root
+// of your application.
+@override
+Widget build(BuildContext context) {
+	return MaterialApp(
+	title: 'PageView',
+	theme: ThemeData(
+		primarySwatch: Colors.blue,
+	),
+	debugShowCheckedModeBanner: false,
+	home: const MyHomePage(),
+	);
+}
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+@override
+_MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+PageController controller=PageController();
+final List<Widget> _list=<Widget>[
+	const Center(child:Pages(text: "Page 1",)),
+	const Center(child:Pages(text: "Page 2",)),
+	const Center(child:Pages(text: "Page 3",)),
+	const Center(child:Pages(text: "Page 4",))
+];
+int _curr=0;
+
+@override
+Widget build(BuildContext context) {
+	return Scaffold(
+	backgroundColor: Colors.grey,
+	appBar:AppBar(
+		title: const Text("GeeksforGeeks"),
+		backgroundColor: Colors.green,
+		actions: <Widget>[
+		Padding(
+			padding: const EdgeInsets.all(3.0),
+			child: Text(
+			"Page: ${_curr+1}/${_list.length}",textScaleFactor: 2,),
+		)
+		],),
+	body: PageView(
+		scrollDirection: Axis.horizontal,
+		
+		// reverse: true,
+		// physics: BouncingScrollPhysics(),
+		controller: controller,
+		onPageChanged: (num){
+		setState(() {
+			_curr=num;
+		});
+		},
+		children: 
+		_list,
+	),
+	floatingActionButton:Row(
+		mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+		children:<Widget>[
+		FloatingActionButton(
+			onPressed: () {	 
+			setState(() {
+			_list.add(
+				const Center(child: Text(
+				"New page", style: TextStyle(fontSize: 35.0))),
+			); 
+			});
+			if(_curr!=_list.length-1) {
+			  controller.jumpToPage(_curr+1);
+			} else {
+			  controller.jumpToPage(0);
+			}
+			},
+	child:const Icon(Icons.add)),
+	FloatingActionButton(
+		onPressed: (){
+		_list.removeAt(_curr);
+		setState(() {
+			controller.jumpToPage(_curr-1);
+		});
+		},
+	child:const Icon(Icons.delete)),
+		]
+	)
+	);
+}
+}
+
+class Pages extends StatelessWidget {
+final text;
+const Pages({super.key, this.text});
+@override
+Widget build(BuildContext context) {
+return Center(
+	child: Column(
+		mainAxisAlignment: MainAxisAlignment.center,
+		children:<Widget>[
+		Text(text,textAlign: TextAlign.center,style: const TextStyle(
+			fontSize: 30,fontWeight:FontWeight.bold),),	 
+		]
+	),
+	);
+}
+}
--- lib/views/projects.dart
+++ lib/views/projects.dart
@@ -1,8 +1,11 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 import '../configuration/constant.dart';
+import '../play_ground.dart';
 
 String servicesDesc =
     "Throughout my career as a designer and developer, I have crafted digital products catering to both commercial and individual needs.";
@@ -10,8 +13,15 @@ String descProd =
     "The SafeMO app is designed for commuters, aiming to provide security measures in partnership with 911 here in Puerto Princesa City.";
 String kSafeMoProject = "assets/safemoApp.png";
 
-class Projects extends StatelessWidget {
+class Projects extends StatefulWidget {
   const Projects({super.key});
+
+  @override
+  State<Projects> createState() => _ProjectsState();
+}
+
+class _ProjectsState extends State<Projects> {
+  final PageController pageViewControll = PageController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,101 +29,135 @@ class Projects extends StatelessWidget {
       height: context.percentHeight * 92,
       width: context.percentWidth * 100,
       decoration: const BoxDecoration(),
-      child: Stack(children: [
-        Align(
-            alignment: Alignment.topCenter,
-            child: Column(
-              children: [
-                Text(
-                  "Projects",
-                  style: kPoppinBold.copyWith(
-                      color: kWhite, fontSize: context.percentWidth * 3),
-                ),
-                Text(
-                  servicesDesc,
-                  textAlign: TextAlign.center,
-                  style: kPoppinSemiBold.copyWith(
-                      color: kLightGrey, fontSize: context.percentWidth * 1.5),
-                ).paddingSymmetric(horizontal: 50),
-              ],
-            )).marginOnly(top: context.percentHeight * 5),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-              decoration: BoxDecoration(
-                  color: kLighBlue, borderRadius: BorderRadius.circular(15)),
-              width: context.percentWidth * 77.083,
-              height: context.percentHeight * 57.88,
-              child: Stack(
+      child: Stack(
+        children: [
+          Align(
+              alignment: Alignment.topCenter,
+              child: Column(
                 children: [
-                  Align(
-                    alignment: Alignment.center,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(
-                            iconSize: 50,
-                            color: kWhite,
-                            onPressed: () {},
-                            icon: const Icon(Icons.arrow_circle_left_outlined)),
-                        IconButton(
-                            iconSize: 50,
-                            color: kWhite,
-                            onPressed: () {},
-                            icon: const Icon(Icons.arrow_circle_right_outlined))
-                      ],
-                    ),
+                  Text(
+                    "Projects",
+                    style: kPoppinBold.copyWith(
+                        color: kWhite, fontSize: context.percentWidth * 3),
                   ),
-                  Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text("SAFEMO APP",
-                                    style: kPoppinExtraBold.copyWith(
-                                        fontSize: context.percentWidth * 2,
-                                        color: kWhite))
-                                .marginOnly(
-                                    left: context.percentWidth * 10,
-                                    top: context.percentHeight * 3.32),
-                          ],
-                        ),
-                        SizedBox(
-                          height: context.percentHeight * 39.59,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              SizedBox(
-                                width: context.percentWidth * 40.9,
-                                height: context.percentHeight * 37.25,
-                                child: Image.asset(
-                                  kSafeMoProject,
-                                  fit: BoxFit.fill,
-                                ),
-                              ),
-                              SizedBox(
-                                width: context.percentWidth * 15.34,
-                                child: Text(descProd,
-                                    style: kPoppinRegular.copyWith(
-                                        fontSize: context.percentWidth * 1.5,
-                                        color: kWhite)),
-                              ),
-                            ],
-                          ),
-                        ),
-                        ElevatedButton(
-                                onPressed: () {},
-                                child: const Text("View Project"))
-                            .marginOnly(bottom: context.percentHeight * 2)
-                      ],
-                    ),
-                  ),
+                  Text(
+                    servicesDesc,
+                    textAlign: TextAlign.center,
+                    style: kPoppinSemiBold.copyWith(
+                        color: kLightGrey,
+                        fontSize: context.percentWidth * 1.5),
+                  ).paddingSymmetric(horizontal: 50),
                 ],
-              )),
-        ).marginOnly(bottom: context.percentHeight * 5.12)
-      ]),
+              )).marginOnly(top: context.percentHeight * 2),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                decoration: BoxDecoration(
+                    color: kLighBlue, borderRadius: BorderRadius.circular(15)),
+                width: context.percentWidth * 77.083,
+                height: context.percentHeight * 57.88,
+                child: Stack(
+                  children: [
+                    PageView.builder(
+                      onPageChanged: (index) {
+                        print('index $index');
+                      },
+                      controller: pageViewControll,
+                      itemCount: 5,
+                      itemBuilder: (context, index) => const ProjectContent(),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: IconButton(
+                          iconSize: 50,
+                          color: kWhite,
+                          onPressed: () {
+                            pageViewControll.nextPage(
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.ease,
+                            );
+                          },
+                          icon: const Icon(Icons.arrow_circle_right_outlined)),
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: IconButton(
+                          iconSize: 50,
+                          color: kWhite,
+                          onPressed: () {
+                            pageViewControll.previousPage(
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.ease,
+                            );
+                          },
+                          icon: const Icon(Icons.arrow_circle_left_outlined)),
+                    ),
+                  ],
+                )),
+          ).marginOnly(bottom: context.percentHeight * 10),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SmoothPageIndicator(
+              controller: pageViewControll,
+              count: 5,
+              effect: ExpandingDotsEffect(dotColor: kWhite),
+            ),
+          ).marginOnly(bottom: context.percentHeight * 5)
+        ],
+      ),
+    );
+  }
+}
+
+class ProjectContent extends StatelessWidget {
+  const ProjectContent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: context.percentHeight * 39.59,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text("SAFEMO APP",
+                      style: kPoppinExtraBold.copyWith(
+                          fontSize: context.percentWidth * 2, color: kWhite))
+                  .marginOnly(
+                      left: context.percentWidth * 10,
+                      top: context.percentHeight * 3.32),
+            ],
+          ),
+          SizedBox(
+            height: context.percentHeight * 39.59,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: context.percentWidth * 40.9,
+                  height: context.percentHeight * 37.25,
+                  child: Image.asset(
+                    kSafeMoProject,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                SizedBox(
+                  width: context.percentWidth * 15.34,
+                  child: Text(descProd,
+                      style: kPoppinRegular.copyWith(
+                          fontSize: context.percentWidth * 1.5, color: kWhite)),
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(onPressed: () {}, child: const Text("View Project"))
+              .marginOnly(bottom: context.percentHeight * 2)
+        ],
+      ),
     );
   }
 }
