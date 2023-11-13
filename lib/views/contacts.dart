8ba38e3afb97d053db5f2ebca67ee9ddac541ddb
Ralph Kevin Rynard E. Macahipay <53963085+ralphkevinmacahipay@users.noreply.github.com>
2023-11-13 19:14:06 +0800

    done contact tablet

--- lib/views/contacts.dart
+++ lib/views/contacts.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_profile/configuration/constant.dart';
 import 'package:my_profile/services/general_services.dart';
+import 'package:my_profile/views/widget_sources.dart';
+import 'package:responsive_framework/responsive_breakpoints.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 String contactDesc =
@@ -12,19 +14,11 @@ class Contacts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print("h: ${context.percentHeight * 100}");
-
     return SizedBox(
       height: context.percentHeight * 92,
       width: context.percentWidth * 100,
       child: Stack(children: [
-        Align(
-            alignment: Alignment.topCenter,
-            child: Text(
-              "Contacts",
-              style: kPoppinBold.copyWith(
-                  color: kWhite, fontSize: context.percentWidth * 3),
-            )).marginOnly(top: context.percentHeight * 2),
+        TitleWidget(servicesDesc: contactDesc, kTitle: "Contacts"),
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
@@ -32,63 +26,90 @@ class Contacts extends StatelessWidget {
                 color: kLighDark, borderRadius: BorderRadius.circular(15)),
             width: context.percentWidth * 88.61,
             height: context.percentHeight * 40.76,
-            child: Column(children: [
-              Text("Stay Connected",
-                  style: kPoppinSemiBold.copyWith(
-                      color: kWhite, fontSize: context.percentWidth * 4.513)),
-              Text(contactDesc,
-                  textAlign: TextAlign.center,
-                  style: kPoppinRegular.copyWith(
-                    color: kWhite,
-                    fontSize: context.percentWidth * 1.3888,
-                  )).marginSymmetric(horizontal: context.percentWidth * 5),
-              Divider(
-                thickness: 2,
-                color: kWhite,
-                height: 10,
-              ).marginSymmetric(horizontal: context.percentWidth * 5),
-              const Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            child: Column(
+                mainAxisAlignment: ResponsiveBreakpoints.of(context).isDesktop
+                    ? MainAxisAlignment.start
+                    : MainAxisAlignment.spaceEvenly,
                 children: [
-                  IconWidgetProvider(kIcon: kFaceBook, link: kMyFaceBook),
-                  IconWidgetProvider(kIcon: kInstagram, link: kMyInstagram),
-                  IconWidgetProvider(kIcon: kGitHub, link: kMyGitHub),
-                  IconWidgetProvider(kIcon: kLinkedIn, link: kMyLinkedIn),
-                ],
-              ).marginOnly(bottom: context.percentHeight * 5)
-            ]),
+                  Text("Stay Connected",
+                      style: kPoppinSemiBold.copyWith(
+                          color: kWhite,
+                          fontSize: context.percentWidth * 4.513)),
+                  ResponsiveBreakpoints.of(context).isDesktop
+                      ? Text(contactDesc,
+                              textAlign: TextAlign.center,
+                              style: kPoppinRegular.copyWith(
+                                color: kWhite,
+                                fontSize: context.percentWidth * 1.3888,
+                              ))
+                          .marginSymmetric(horizontal: context.percentWidth * 5)
+                      : const SizedBox.shrink(),
+                  Divider(
+                    thickness: 2,
+                    color: kWhite,
+                    height: 10,
+                  ).marginSymmetric(horizontal: context.percentWidth * 5),
+                  ResponsiveBreakpoints.of(context).isDesktop
+                      ? const Spacer()
+                      : const SizedBox.shrink(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconWidgetProvider(kIcon: kFaceBook, link: kMyFaceBook),
+                      IconWidgetProvider(kIcon: kInstagram, link: kMyInstagram),
+                      IconWidgetProvider(kIcon: kGitHub, link: kMyGitHub),
+                      IconWidgetProvider(kIcon: kLinkedIn, link: kMyLinkedIn),
+                    ],
+                  ).marginOnly(bottom: context.percentHeight * 5)
+                ]),
           ),
         ).marginOnly(bottom: context.percentHeight * 10),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: RichText(
-            text: TextSpan(
-              text: 'Developed by ',
-              style: kPoppinRegular.copyWith(
-                color: kWhite,
-                fontSize: context.percentWidth * 1,
-              ),
-              children: [
-                TextSpan(
-                  text: 'Ralph Kevin Rynard E. Macahipay',
-                  style: kPoppinBold.copyWith(
-                    color: kWhite,
-                    fontSize: context.percentWidth * 1,
-                  ),
-                ),
-                TextSpan(
-                  text: ' © 2023',
-                  style: kPoppinRegular.copyWith(
-                    color: kWhite,
-                    fontSize: context.percentWidth * 1,
-                  ),
-                ),
-              ],
-            ),
-          ),
+        CopyRigthWidget(
+          kFontSize: ResponsiveBreakpoints.of(context).isDesktop
+              ? context.percentWidth * 1
+              : context.percentWidth * 2,
         )
       ]),
+    );
+  }
+}
+
+class CopyRigthWidget extends StatelessWidget {
+  final double kFontSize;
+  const CopyRigthWidget({
+    super.key,
+    required this.kFontSize,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: RichText(
+        text: TextSpan(
+          text: 'Developed by ',
+          style: kPoppinRegular.copyWith(
+            color: kWhite,
+            fontSize: kFontSize,
+          ),
+          children: [
+            TextSpan(
+              text: 'Ralph Kevin Rynard E. Macahipay',
+              style: kPoppinBold.copyWith(
+                color: kWhite,
+                fontSize: kFontSize,
+              ),
+            ),
+            TextSpan(
+              text: ' © 2023',
+              style: kPoppinRegular.copyWith(
+                color: kWhite,
+                fontSize: kFontSize,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -106,7 +127,9 @@ class IconWidgetProvider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      iconSize: context.percentWidth * 2.472,
+      iconSize: ResponsiveBreakpoints.of(context).isDesktop
+          ? context.percentWidth * 2.472
+          : context.percentWidth * 10,
       onPressed: () =>
           GeneralServices().openURL(uri: GeneralServices().parseURL(url: link)),
       icon: Image.asset(kIcon, fit: BoxFit.cover),
